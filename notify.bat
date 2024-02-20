b1bd70b1c565b1859587befe32e5778ecf3c2c0e
tomato645 <89794096+tomato645@users.noreply.github.com>
2024-02-21 03:58:11 +0900

    Update

--- notify.bat
+++ notify.bat
@@ -1,14 +1,14 @@
 @echo off
 setlocal
-set folder=%1
-set MaxNum=%2
+set folder_path=%1
+set max_num=%2
 :loop
-set count=0
-for /f %%A in ('dir /a-d-s-h /b "%folder%" ^| find /v /c ""') do set count=%%A
-echo %folder%には%count%個のファイルがあります。
-if %count% equ %MaxNum% (
+set file_count=0
+for /f %%A in ('dir /a-d-s-h /b "%folder_path%" ^| find /v /c ""') do set file_count=%%A
+echo %folder_path% には %file_count% 個のファイルがあります。
+if %file_count% equ %max_num% (
   echo 終了します。
-  msg * %folder%のファイル数が%MaxNum%に達しました。
+  msg * %folder_path% のファイル数が %max_num% に達しました。
   exit /b
 )
 timeout /t 5 /nobreak >nul
